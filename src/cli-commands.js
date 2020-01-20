6280ed292aab85e9613b22cd1c6dde1fcc2d3643
Oleg Legun <oleg.legun@gmail.com>
2020-01-20 17:50:43 +0300

    add CLI commands

---
+++ src/cli-commands.js
@@ -0,0 +1,32 @@
+const Commands = {
+    INIT: {
+        name: 'init',
+        description: 'Initialize project creating necessary files and directories',
+    },
+    GENERATE: {
+        name: 'generate',
+        description: 'Compile and generate static site files',
+        alias: 'start',
+    },
+    CONFIGURATION: {
+        name: 'config',
+        description: 'Print current generator configuration',
+    },
+    SERVE: {
+        name: 'serve',
+        description: 'Start development server in distribution folder',
+    },
+    BOOTSTRAP: {
+        name: 'bootstrap',
+        description: 'Generate simple test Bootstrap project in current directory'
+    },
+    HELP: {
+        name: 'help',
+        description: 'Show help information',
+        alias: 'h',
+    },
+}
+
+module.exports = {
+    Commands,
+}
