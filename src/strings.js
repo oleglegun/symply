b2a05b79b70a641469495007bf92ed6b6a0e9c38
Oleg Legun <oleg.legun@gmail.com>
2020-01-20 17:54:41 +0300

    add templates entity; remove info from strings; improve configuration;

--- src/strings.js
+++ src/strings.js
@@ -1,23 +1,11 @@
 module.exports = {
     logo: `
-███████╗██╗   ██╗███╗   ███╗██████╗ ██╗  ██╗   ██╗
-██╔════╝╚██╗ ██╔╝████╗ ████║██╔══██╗██║  ╚██╗ ██╔╝
-███████╗ ╚████╔╝ ██╔████╔██║██████╔╝██║   ╚████╔╝ 
-╚════██║  ╚██╔╝  ██║╚██╔╝██║██╔═══╝ ██║    ╚██╔╝  
-███████║   ██║   ██║ ╚═╝ ██║██║     ███████╗██║   
-╚══════╝   ╚═╝   ╚═╝     ╚═╝╚═╝     ╚══════╝╚═╝   
- ======= Bootstrap Static Site Generator =======
-`,
-    help: `
-Usage: symply <command> [options]
-
-Available commands:
-
-symply init          Initialize project, creating necessary files and directories
-
-symply generate      Complile and generate static site files
-alias: symply start
-
-symply serve         Start development server
+  ███████╗ ████████╗ ███╗     ███╗ ████████╗  ██╗   ████████╗
+  ████▌/═╝ ╚██████╔╝ ████╗   ████║ ███╔═══██╗ ██║   ╚██████╔╝
+  ███████╗  ╚████╔╝  █████╗ █████║ ████████╔╝ ██║    ╚████╔╝ 
+  ╚/▐████║   ╚██╔╝   ████████████║ ███╔════╝  ██║     ╚██╔╝  
+  ███████║    ██║    ████████████║ ███║       ███████╗ ██║   
+  ╚══════╝    ╚═╝    ╚═══════════╝ ╚══╝       ╚══════╝ ╚═╝   
+      ======= Bootstrap Static Site Generator =======
 `,
 }
