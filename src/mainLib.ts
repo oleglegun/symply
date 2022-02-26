64125cb7ca76809d5d15565dc8fdf20c1f2f2883
Oleg Legun <oleg.legun@gmail.com>
2022-02-26 21:24:02 +0300

    add generation time output message; update dependencies

--- src/mainLib.ts
+++ src/mainLib.ts
@@ -4,10 +4,11 @@ import logger from './logger'
 import chalk from 'chalk'
 
 async function generate(configuration: SymplyConfiguration) {
+    const start = Date.now()
     config.setModuleModeConfiguration(configuration)
     logger.info(chalk.magentaBright('SYMPLY::GENERATE'), chalk.greenBright('[START]'))
     await generateCommand()
-    logger.info(chalk.magentaBright('SYMPLY::GENERATE'), chalk.greenBright('[DONE]'))
+    logger.info(chalk.magentaBright('SYMPLY::GENERATE'), chalk.greenBright(`[DONE in ${(Date.now() - start) / 1000}s]`))
 }
 
 module.exports = generate
